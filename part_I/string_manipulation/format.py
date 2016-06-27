74b70494b931bdb7f5976fa5c1a7f2ec248f5db2
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2016-06-27 14:56:26 -0700

    add string manipulation chapter

---
+++ part_I/string_manipulation/format.py
@@ -0,0 +1,5 @@
+year_started = "1989"
+creator = "Guido van Rossum"
+country = "the Netherlands"
+my_string = "Python was created in {} by {} in {}.".format(year_started, creator, country)
+print(my_string)
