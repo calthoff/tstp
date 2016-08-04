38d8fab82679a72a254cc71ca13f9466b1fb1978
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2016-08-03 20:10:41 -0700

    add e

---
+++ part_II/more_object_oriented_programming/overriding_builtin.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # def __repr__(self):
+    #     return self.name
+
+joe = Person('Joe', 32)
+print(joe)
