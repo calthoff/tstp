be4535482aeaf6e5d1111b601327bf2ca3839ed9
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2016-10-14 12:35:06 -0700

    new examples

---
+++ part_I/functions/before_defined.py
@@ -0,0 +1,4 @@
+if x > 100:
+    print("x is greater than 100")
+
+x = 110
---
+++ part_I/functions/save_variable.py
@@ -0,0 +1,9 @@
+def f(x):
+    return x + 1
+
+z = f(4)
+
+if z == 5:
+    print("z is 5")
+else:
+    print("z is not 5")
