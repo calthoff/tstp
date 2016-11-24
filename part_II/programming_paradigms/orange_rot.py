c182c8cce6650b2ca7a7a95b7672e77ec1176211
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2016-11-23 17:48:09 -0800

    orange rot

---
+++ part_II/programming_paradigms/orange_rot.py
@@ -0,0 +1,15 @@
+class Orange():
+    def __init__(self):
+        """all weights are in oz"""
+        self.weight = 6
+        self.color = 'orange'
+        self.mold = 0
+
+    def rot(self, days, temperature):
+        self.mold = days * (temperature * .1)
+
+
+orange = Orange()
+print(orange.mold)
+orange.rot(10, 98)
+print(orange.mold)
