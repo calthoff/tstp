9ca7a2da47b9a407062c52b388e3ffd7193a4332
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2016-11-23 17:58:33 -0800

    new ex

---
+++ part_II/the_four_pillars_of_oop/rectangle.py
@@ -0,0 +1,7 @@
+class Rectangle():
+    def __init__(self, width, length):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.width * self.length
