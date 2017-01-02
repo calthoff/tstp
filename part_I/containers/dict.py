9a10603c2d99327384fd31127f79a9a79966bcf0
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-02 11:49:01 -0800

    more outdated comments

--- part_I/containers/dict.py
+++ part_I/containers/dict.py
@@ -1,3 +1,10 @@
+# IF YOU ARE READING THIS YOU ARE READING
+# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
+# IS MUCH BETTER.
+# I am working with Amazon to resolve this.
+# Please email me at cory@theselftaughtprogrammer.io
+# For an updated version
+
 songs = {"Like a Rolling Stone": "Bob Dylan",
          "A Day in the Life": "The Beatles",
          "Here Comes the Sun": "The Beatles",
