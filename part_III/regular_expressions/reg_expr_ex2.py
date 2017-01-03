728d409db25bc99a3543ee775cf8e6814b88d838
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-02 23:26:58 -0800

    outdated part III

--- part_III/regular_expressions/reg_expr_ex2.py
+++ part_III/regular_expressions/reg_expr_ex2.py
@@ -1,3 +1,11 @@
+# IF YOU ARE READING THIS YOU ARE READING 
+# AN OUTDATED VERSION OF THE BOOK.
+# I am working with Amazon to resolve this.
+# The new version is much better and has correctly formatted code examples
+# In the book.
+# Please email me at cory@theselftaughtprogrammer.io
+# For an updated version
+
 import re
 
 line = """The numbers 172 can be found on the back of the U.S. $5 dollar bill in the bushes at the base
