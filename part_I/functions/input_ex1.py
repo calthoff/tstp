84a7f11298141b8c21ff364b9898f121572a6b69
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:19:30 -0800

    change comment

--- part_I/functions/input_ex1.py
+++ part_I/functions/input_ex1.py
@@ -1,9 +1,10 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
+# I shortened the message that gets printed in this example.
+# The example in older versions of the book prints "How old are you?"
+# I did this so the example will fit better on smaller devices.
+# If you have an older version of the book, you can email
+# me at cory@theselftaughtprogrammer.io 
+# to update to the newest version. Thank you so much for reading!
 
 age = input("How old are you?")
 int_age = int(age)
