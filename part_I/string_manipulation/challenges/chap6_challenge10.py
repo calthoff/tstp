79fccfd60954d5c504e6143ade76f46d552b4b2c
darkshadowviperx <darkshadowviperx@gmail.com>
2017-06-26 05:35:28 -0700

    Update chap6_challenge10.py
    Suggestion: dynamic solution to challenge 10 as opposed to static "33" index of ","
    Thanks.

--- part_I/string_manipulation/challenges/chap6_challenge10.py
+++ part_I/string_manipulation/challenges/chap6_challenge10.py
@@ -1,3 +1,9 @@
+# static solution
 sentence = "It was a bright cold day in April, and the clocks were striking thirteen."
 slce = sentence[0:33]
+print(slce)
+
+#dynamic solution
+sentence = "It was a bright cold day in April, and the clocks were striking thirteen."
+slce = test_str[:test_str.index(",")] 
 print(slce)
