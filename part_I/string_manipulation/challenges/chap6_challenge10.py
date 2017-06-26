0f9e9fb1ef0068d0d0677a9203c281d63dafb55b
darkshadowviperx <darkshadowviperx@gmail.com>
2017-06-26 05:38:55 -0700

    Update chap6_challenge10.py
    Whoops! Forgot to change variable names to be consistent with original solution.

--- part_I/string_manipulation/challenges/chap6_challenge10.py
+++ part_I/string_manipulation/challenges/chap6_challenge10.py
@@ -5,5 +5,5 @@ print(slce)
 
 #dynamic solution
 sentence = "It was a bright cold day in April, and the clocks were striking thirteen."
-slce = test_str[:test_str.index(",")] 
+slce = sentence[:sentence.index(",")] 
 print(slce)
