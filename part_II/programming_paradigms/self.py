e69faeeac55b59bca6edab43a1f74421a8e1ae23
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:46:04 -0700

    Update self.py

--- part_II/programming_paradigms/self.py
+++ part_II/programming_paradigms/self.py
@@ -1,10 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING 
-# AN OUTDATED VERSION OF THE BOOK.
-# I am working with Amazon to resolve this.
-# The new version is much better and has correctly formatted code examples
-# In the book.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 class Orange:
     print("Orange created!")
