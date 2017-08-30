34d26aecf5cfa50197391c4e93170015d7f78033
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:30:41 -0700

    Update scope2.py

--- part_I/functions/scope2.py
+++ part_I/functions/scope2.py
@@ -1,11 +1,6 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
 
-ef f():
+
+def f():
    x = 1
    y = 2
    z = 3
