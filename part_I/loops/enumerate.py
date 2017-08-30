4d259f6afb6035912f46bd1568f6de869776e5ea
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:37:36 -0700

    Update enumerate.py

--- part_I/loops/enumerate.py
+++ part_I/loops/enumerate.py
@@ -1,9 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 intense_shows = ["Game of Thrones", "Narcos", "Vice"]
 for i, show in enumerate(intense_shows):
