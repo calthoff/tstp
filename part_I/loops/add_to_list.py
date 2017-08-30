1534112cd25d8ec138948c29ec9b36af97a004d2
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:35:28 -0700

    Update add_to_list.py

--- part_I/loops/add_to_list.py
+++ part_I/loops/add_to_list.py
@@ -1,9 +1,3 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
 
 intense_shows = ["Game of Thrones", "Narcos", "Vice"]
 comedies = ["Arrested Development", "How I Met Your Mother", "Always Sunny"]
