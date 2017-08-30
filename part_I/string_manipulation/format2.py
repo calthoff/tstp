46a29ec1b42be8b26347a7aed1582aea9877fe54
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:41:30 -0700

    Update format2.py

--- part_I/string_manipulation/format2.py
+++ part_I/string_manipulation/format2.py
@@ -1,9 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 author = "walter Isaacson"
 
