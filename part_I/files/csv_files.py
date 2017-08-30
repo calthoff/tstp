4d7a6903624ed3da5a17e3301f67f863f7641e83
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:23:00 -0700

    Update csv_files.py

--- part_I/files/csv_files.py
+++ part_I/files/csv_files.py
@@ -1,9 +1,3 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
 
 import csv
 
