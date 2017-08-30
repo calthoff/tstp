b3551b88477736e17b7f3c97262396d511d8fe55
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:22:14 -0700

    Update lists_of_lists.py

--- part_I/containers/lists_of_lists.py
+++ part_I/containers/lists_of_lists.py
@@ -1,11 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
 
-# ex1
 
 list_of_lists = []
 rappers = ["Kanye West", "Jay Z", "Eminem", "Nas"]
