3dc6ad1e352999ce82dc14ec9b60f9e3256f3070
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:55:18 -0700

    Update fizz_buzz.py

--- part_IV/algorithms/fizz_buzz.py
+++ part_IV/algorithms/fizz_buzz.py
@@ -1,10 +1,4 @@
-F YOU ARE READING THIS YOU ARE READING 
-# AN OUTDATED VERSION OF THE BOOK.
-# I am working with Amazon to resolve this.
-# The new version is much better and has correctly formatted code examples
-# In the book.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 def fizz_buzz():
     for i in range(1, 101):
