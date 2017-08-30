34d7342d50bef258182a70ec6ca94a997ae287d8
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:25:28 -0700

    Update df_ex2.py

--- part_I/functions/df_ex2.py
+++ part_I/functions/df_ex2.py
@@ -1,8 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING 
-# AN OUTDATED VERSION OF THE BOOK.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 def f(x):
     return x * 2
