54d48aaaf5bd07cb003e01be76a44a0c3076f453
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:37:56 -0700

    Update for_loop_ex4.py

--- part_I/loops/for_loop_ex4.py
+++ part_I/loops/for_loop_ex4.py
@@ -1,9 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 md = {"key1": "value1", "key2": "value2"}
 for key in md:
