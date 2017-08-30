cb69c87c66a486e5a637fc4affe9e39b9c445fe4
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:33:26 -0700

    Update control_flow_ex8.py

--- part_I/introduction_to_programming/control_flow_ex8.py
+++ part_I/introduction_to_programming/control_flow_ex8.py
@@ -1,8 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING 
-# AN OUTDATED VERSION OF THE BOOK.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 x = 100
 if x == 10:
