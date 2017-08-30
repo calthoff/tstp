4c1d2a07e3e09b4b7065bf9cf02a30895c4c1370
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 19:00:52 -0700

    Update dry_ex1.py

--- part_V/good_programming_practices/dry_ex1.py
+++ part_V/good_programming_practices/dry_ex1.py
@@ -1,10 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING 
-# AN OUTDATED VERSION OF THE BOOK.
-# I am working with Amazon to resolve this.
-# The new version is much better and has correctly formatted code examples
-# In the book.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 def upper(word, word_list):
     for index, item in enumerate(word_list):
