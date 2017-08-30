ccd30180ea356cb0040bf27c209508f74eb354b6
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-08-29 18:22:03 -0700

    Update list_in_tup.py

--- part_I/containers/list_in_tup.py
+++ part_I/containers/list_in_tup.py
@@ -1,9 +1,4 @@
-# IF YOU ARE READING THIS YOU ARE READING
-# AN OUTDATED VERSION OF THE BOOK. THE NEW VERSION
-# IS MUCH BETTER.
-# I am working with Amazon to resolve this.
-# Please email me at cory@theselftaughtprogrammer.io
-# For an updated version
+
 
 eighteen_hundreds = ["Edgar Allen Poe", "Charles Dickens"]
 nineteen_hundreds = ["Hemingway", "Fitzgerald", "Orwell"]
